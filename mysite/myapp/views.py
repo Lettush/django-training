0e8deae7a2ffb75351f1e1cc347a4d5b88f6830d
Lettush <jaredneilhortaleza11@gmail.com>
2023-11-14 14:13:10 +0800

    initial commit, added list and detail view

---
+++ mysite/myapp/views.py
@@ -0,0 +1,29 @@
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.utils import timezone
+from .models import Person
+
+
+class PersonListView(ListView):
+    model = Person
+    paginate_by = 100
+    template_name = "myapp/person.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now
+        return context
+
+
+class PersonDetailView(DetailView):
+    model = Person
+    template_name = "myapp/person_detail.html"
+
+
+"""
+    'CreateView' - A view that displays a form for creating an object, redisplaying the form with validation errors (if there are any) and saving the object.
+    'DeleteView' - A view that displays a confirmation page and deletes an existing object. 
+    'DetailView' - A view for a specific object based on its ID
+    'ListView' - Show a list/array of objects in a View
+    'UpdateView' - A view that displays a form for updating an object, redisplaying the form with validation errors (if there are any) and saving the object.
+"""
